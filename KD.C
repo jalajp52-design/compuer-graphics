a4f035ad5ad05e23fd639329d40afa33d2fda239
jalajp52-design <jalajp52@gmail.com>
2026-01-29 10:38:55 +0530

    Add files via upload

--- KD.C
+++ KD.C
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <conio.h>
-#include <graphics.h>
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
 
 void bresenhamAlgorithm(int x1, int y1, int x2, int y2) {
     int deltax = x2 - x1;
@@ -8,45 +8,30 @@ void bresenhamAlgorithm(int x1, int y1, int x2, int y2) {
     int p = 2 * deltay - deltax;
     int x = x1;
     int y = y1;
-
-    
     putpixel(x, y, WHITE);
-
-  
-    if (deltay > deltax) {
-    
-        int temp = x;
-        x = y;
-        y = temp;
-
-      
-        deltax = x2 - x1;
-        deltay = y2 - y1;
-    }
-
     while (x <= x2) {
-     
-        putpixel(x, y, WHITE);
 
-       
-        if (p < 0) {
-            x++;
-            p = p + 2 * deltay;
-        }
-    
-        else {
-            x++;
-            y++;
-            p = p + 2 * (deltay - deltax);
-        }
+	putpixel(x, y, WHITE);
+
+
+	if (p < 0) {
+	    x++;
+	    p = p + 2 * deltay;
+	}
+
+	else {
+	    x++;
+	    y++;
+	    p = p + 2 * (deltay - deltax);
+	}
     }
 }
 
 int main() {
-    int gd = DETECT, gmode;
-    initgraph(&gd, &gmode, "C:\\Turboc3\\BGI");
 
-    int x1, y1, x2, y2;
+    int gd=0, gmode;
+    int x1,x2,y1,y2;
+    initgraph(&gd, &gmode, "c:\\tc\\bgi");
     printf("Enter x1: ");
     scanf("%d", &x1);
     printf("Enter y1: ");
